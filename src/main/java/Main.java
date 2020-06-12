aab733a2dd916e6b9b988cfeb18b0f320287b1f2
Lucca Romaniello <luccaromaniello1@gmail.com>
2020-06-12 00:16:18 -0300

    n392

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,32 +2,40 @@ import models.DirectedGraph;
 import utils.FileManager;
 
 import java.io.IOException;
-
-import static java.time.temporal.ChronoUnit.MINUTES;
-
+import java.time.LocalTime;
 
 public class Main {
     public static void main(String[] args) {
 
         // SHORTEST PATH EXAMPLE
-        FileManager fileManager = new FileManager("bigGraph.txt");
+//        FileManager fileManager = new FileManager("bigGraph.txt");
+//        try {
+//            DirectedGraph graph = fileManager.readDirectedGraphFile();
+//            graph.calculateShortestPathFromSource("D", "A");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //THIRD PROBLEM EXAMPLE
+//        FileManager fileManager2 = new FileManager("stronglyConnectedGraph.txt");
+//        FileManager fileManager3 = new FileManager("notStronglyConnectedGraph.txt");
+//        try {
+//            DirectedGraph scg = fileManager2.readDirectedGraphFile();
+//            DirectedGraph nscg = fileManager3.readDirectedGraphFile();
+//            scg.thirdProblem();
+//            nscg.thirdProblem();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        // FOURTH PROBLEM
+        FileManager fileManager4 = new FileManager("lastFlight.txt");
         try {
-            DirectedGraph graph = fileManager.readDirectedGraphFile();
-            graph.calculateShortestPathFromSource("D", "A");
+            DirectedGraph graph = fileManager4.readDirectedGraphFile();
+            graph.lastAvailableFlight("CONFINS", "GUARULHOS", LocalTime.of(6, 10));
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        //THIRD PROBLEM EXAMPLE
-        FileManager fileManager2 = new FileManager("stronglyConnectedGraph.txt");
-        FileManager fileManager3 = new FileManager("notStronglyConnectedGraph.txt");
-        try {
-            DirectedGraph scg = fileManager2.readDirectedGraphFile();
-            DirectedGraph nscg = fileManager3.readDirectedGraphFile();
-            scg.thirdProblem();
-            nscg.thirdProblem();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
